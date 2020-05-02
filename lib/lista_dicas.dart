f89e01c1527cc3c6a97e32b0fc9ba127ad9556d7
ailsonazevedo <61508532+ailsonazevedo@users.noreply.github.com>
2020-05-02 17:39:36 -0300

    Update nas imagens

--- lib/lista_dicas.dart
+++ lib/lista_dicas.dart
@@ -9,7 +9,7 @@ class Dica {
 List dicas = [
   Dica(
     imageUrl:
-        'https://d36tnp772eyphs.cloudfront.net/blogs/1/2018/02/Christ-the-Redeemer.jpg',
+        'https://www.rbsdirect.com.br/imagesrc/17383078.jpg',
     title:
         'Lavar as mãos é uma das principais formas de prevenção contra o Coronavírus',
     text:
@@ -17,9 +17,9 @@ List dicas = [
   ),
   Dica(
     imageUrl:
-        "https://d36tnp772eyphs.cloudfront.net/blogs/1/2018/02/Christ-the-Redeemer.jpg",
+        "https://abrilsuperinteressante.files.wordpress.com/2020/02/corona-no-brasil-5-perguntas-site.png",
     title: 'Use uma máscara ao sair em público',
-    text: "Christ the Redeemer",
+    text: "Em meio a atual crise pandêmica do novo coronavírus (COVID-19), o Ministério da Saúde (MS) recomenda à população o uso de máscaras, incluindo as de tecido. Usar máscaras de tecido é uma alternativa de grande importância para a população saudável, pois bloqueia duas portas de entrada do coronavírus: a boca e o nariz. É importante ressaltar que a máscara de tecido deve ser lavada após o uso, para evitar transporte de doenças para a sua casa.",
   ),
   Dica(
       imageUrl:
