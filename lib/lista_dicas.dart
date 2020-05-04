b28b8b77a01fd4f3ac965be5b166f228ef36ce84
ailsonazevedo <61508532+ailsonazevedo@users.noreply.github.com>
2020-05-04 20:13:55 -0300

    Update das imagens
    Colocando outras imagens

--- lib/lista_dicas.dart
+++ lib/lista_dicas.dart
@@ -9,7 +9,7 @@ class Dica {
 List dicas = [
   Dica(
     imageUrl:
-        'https://www.rbsdirect.com.br/imagesrc/17383078.jpg',
+        'https://image.freepik.com/vetores-gratis/rapaz-com-cabelo-azul-lavando-as-maos_23-2148503809.jpg',
     title:
         'Lavar as mãos é uma das principais formas de prevenção contra o Coronavírus',
     text:
@@ -17,24 +17,24 @@ List dicas = [
   ),
   Dica(
     imageUrl:
-        "https://abrilsuperinteressante.files.wordpress.com/2020/02/corona-no-brasil-5-perguntas-site.png",
+        "https://image.freepik.com/vetores-gratis/pessoas-vestindo-mascara-medica_23-2148469659.jpg",
     title: 'Use máscara ao sair em público',
     text: "Em meio a atual crise pandêmica do novo coronavírus (COVID-19), o Ministério da Saúde (MS) recomenda à população o uso de máscaras, incluindo as de tecido. Usar máscaras de tecido é uma alternativa de grande importância para a população saudável, pois bloqueia duas portas de entrada do coronavírus: a boca e o nariz. É importante ressaltar que a máscara de tecido deve ser lavada após o uso, para evitar transporte de doenças para a sua casa.",
   ),
   Dica(
       imageUrl:
-          "https://cidadeverde.com/assets/uploads/posts/limpeza-celular-coronavirus-robertaaline.jpg",
-      title: 'Lempre sempre de higienizar objetos pessoais',
+          "https://image.freepik.com/vetores-gratis/mao-segurando-e-limpando-a-tela-do-telefone-movel-com-um-guardanapo-em-estilo-simples-fique-seguro-para-proteger-o-coronavirus-covid-19-surto-de-pandemia-e-conceito-de-ataque_144352-80.jpg",
+      title: 'Lempre-se de sempre higienizar objetos pessoais',
       text: "Celulares, notebooks e outros objetos de uso pessoal devem ser limpos com frequência. "),
   Dica(
       imageUrl:
-          "https://img.freepik.com/fotos-gratis/correio-entregador-de-mascara-protetora-e-luvas-medicas-oferece-comida-e-cafe-para-viagem_120485-4330.jpg",
-      title: 'Matenha distancia ao entregar produtos',
-      text: "Use sempre luvas, sacolas e máscaras descartaveis ao entregar ou receber produtos, pois isso dificulta a transmissão do vírus."),
+          "https://image.freepik.com/vetores-gratis/fila-de-supermercado-com-distancia-de-seguranca_23-2148520376.jpg",
+      title: 'Matenha distância na compra e na entrega de produtos',
+      text: "Use sempre luvas, sacolas e máscaras descartaveis ao entregar ou receber produtos, desta forma você garante a segurança dos demais e dificulta a transmissão do vírus."),
   Dica(
       imageUrl:
-          "https://d36tnp772eyphs.cloudfront.net/blogs/1/2018/02/View-of-the-Colosseum.jpg",
-      title: 'sdfsdfs',
+          "https://image.freepik.com/vetores-gratis/desinfetante-para-as-maos-estilo-simples_23-2148491719.jpg",
+      title: 'Use sempre álcool em gel',
       text: "The Colosseum"),
   Dica(
       imageUrl:
