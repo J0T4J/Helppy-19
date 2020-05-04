ccccfdffea90502f0ef6073b104eef2246b9a42c
ailsonazevedo <61508532+ailsonazevedo@users.noreply.github.com>
2020-05-04 20:23:28 -0300

    continuação update passado

--- lib/lista_dicas.dart
+++ lib/lista_dicas.dart
@@ -25,12 +25,12 @@ List dicas = [
       imageUrl:
           "https://image.freepik.com/vetores-gratis/mao-segurando-e-limpando-a-tela-do-telefone-movel-com-um-guardanapo-em-estilo-simples-fique-seguro-para-proteger-o-coronavirus-covid-19-surto-de-pandemia-e-conceito-de-ataque_144352-80.jpg",
       title: 'Lempre-se de sempre higienizar objetos pessoais',
-      text: "Celulares, notebooks e outros objetos de uso pessoal devem ser limpos com frequência. "),
+      text: "Celulares, notebooks e outros objetos de uso pessoal devem ser limpos com frequência. Aparelhos eletrônicos devem ser limpos com álcool isopropílico."),
   Dica(
       imageUrl:
           "https://image.freepik.com/vetores-gratis/fila-de-supermercado-com-distancia-de-seguranca_23-2148520376.jpg",
       title: 'Matenha distância na compra e na entrega de produtos',
-      text: "Use sempre luvas, sacolas e máscaras descartaveis ao entregar ou receber produtos, desta forma você garante a segurança dos demais e dificulta a transmissão do vírus."),
+      text: "Use sempre luvas, sacolas e máscaras descartaveis ao entregar ou receber produtos, desta forma você garante a segurança dos demais e dificulta a propagação do vírus."),
   Dica(
       imageUrl:
           "https://image.freepik.com/vetores-gratis/desinfetante-para-as-maos-estilo-simples_23-2148491719.jpg",
