6cbd1e3eb9e39035a33bb5d31f102c899f1798d1
J0T4J <48539986+J0T4J@users.noreply.github.com>
2020-05-05 05:19:42 -0300

    adição de videos

--- lib/lista_videos.dart
+++ lib/lista_videos.dart
@@ -7,10 +7,18 @@ class Video {
 
 List videos = [
   Video(
-    videoUrl: 'https://www.youtube.com/watch?v=C83jhuQMWqc',
-    title: 'É necessário a higienização correta das mãos no combate ao COVID-19!',
+    videoUrl: 'https://www.youtube.com/watch?v=BO0y7FPO3tk',
+    title: 'Dúvidas? Chama o Ministério da Saúde no WhatsApp!',
   ),
-
+  Video(
+    videoUrl: 'https://www.youtube.com/watch?v=6FFsEjUjHoc',
+    title: 'Coronavírus: O que você precisa saber e fazer.',
+  ),
+  Video(
+    videoUrl: 'https://www.youtube.com/watch?v=C83jhuQMWqc',
+    title:
+        'É necessário a higienização correta das mãos no combate ao COVID-19!',
+  ),
   Video(
     videoUrl: 'https://www.youtube.com/watch?v=IlqwgyhBE0w',
     title: 'Suspeita? Se consulte mesmo sem sair de casa!',
